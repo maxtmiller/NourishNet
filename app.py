2a0e54bd2ff65a8f695d52204bd61f24899024d0
Max Miller <67687590+maxtmiller@users.noreply.github.com>
2025-02-28 00:51:22 -0500

    update

--- app.py
+++ app.py
@@ -1017,6 +1017,5 @@ def google_signin():
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port="3000", debug=True)
     port = int(os.getenv('PORT', 5000))
     socketio.run(app, host="0.0.0.0", port=port, debug=False)
